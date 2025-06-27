5cb090ca404aa500a34cc40f802089518645a8b7
Rajashree <rajashree@ecrio.com>
2025-06-27 15:35:58 +0530

    Finished read receipts for images too

---
+++ src/Components/TimeStamp/TimeStamp.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let time = "";
+  const date = new Date(time);
+  const formattedTime = date.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+    timeZone: "IST",
+  });
+</script>
+
+<span>{formattedTime}</span>
+
+<style>
+  span {
+    color: #fff;
+    align-self: flex-end;
+    font-size: 0.6rem;
+  }
+</style>
